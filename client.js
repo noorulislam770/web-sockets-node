818aaec92aa2e4456beb87ff49f7b1a74256d037
Noor Ul Islam <noorulislam770@gmail.com>
2024-05-27 08:13:08 +0500

    added the chat  functionality with the help of packages like socket-io for the server created using express and http an socket.io-client on the front

--- client.js
+++ client.js
@@ -1,8 +1,27 @@
 import socketIoClient from "socket.io-client";
+import * as readline from "node:readline/promises";
+import { response } from "express";
 
 let socket = socketIoClient("http://127.0.0.1:8080");
 
-socket.on("greeting", (data) => {
-  console.log(data);
-  socket.emit("greeting", "hello from the client ");
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+
+socket.on("incoming message", (message) => {
+  console.log("\nrecieved a new message: ", message);
+  //   console.log(message);
+  //   socket.emit("greeting", "hello from the client ");
+});
+
+async function startApp() {
+  while (true) {
+    let response = await rl.question(
+      'Enter a message and hit "return " to send : '
+    );
+    socket.emit("new message", response);
+  }
+}
+
+startApp();
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 import express from "express";
 import { Server } from "socket.io";
 import http from "http";
+import readline from "readline";
 
 let app = express();
 let server = http.createServer(app);
 let io = new Server(server);
 
 io.on("connection", (socket) => {
-  socket.emit("greeting", "hello client ");
   console.log("a new client has connected!");
+  socket.emit("greeting", "hello client ");
 
-  socket.on("greeting", (data) => {
-    console.log(data);
+  socket.on("new message", (message) => {
+    console.log("the client says: ", message);
+    socket.broadcast.emit("incoming message", message);
   });
 });
 
